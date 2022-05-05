d4f078c7b8f018f267a83c9f78d71a3b61db07f5
takingupspace <belllabenthusiast@gmail.com>
2022-05-05 11:55:41 -0500

    added-sentiment-analysis-for-query-page

--- server/models/query.model.js
+++ server/models/query.model.js
@@ -8,6 +8,10 @@ const Query = function(query){
 Query.getData = (req, result) => {
     let table = req.body.table;
     let tableOffset = req.body.offset;
+    console.log("req.body.secondTable = " + req.body.secondTable)
+    console.log("req.body.firstTable " + req.body.table)
+
+    if(req.body.table === "word_count_analysis"){
 
     sql.query(`SELECT * FROM ${table} LIMIT 20 OFFSET ${tableOffset}`, (err, res) => {
         if(err){
@@ -18,6 +22,20 @@ Query.getData = (req, result) => {
             return result(null, res);
         }
     })
+ }else{
+     sql.query(`SELECT od.row_id as original_row_id, od.sentiment_score as original_sentiment,
+     sa.sentiment_score as our_analysis_sentiment_score
+     FROM original_dataset od INNER JOIN sentiment_analysis sa ON
+     od.row_id = sa.row_id LIMIT 20 OFFSET ${tableOffset}`, (err, res) => {
+         if(err){
+             console.log('error in the query model');
+             return result('error in query model', null);
+         }else{
+             console.log('query was successful in the query model')
+             return result(null, res);
+         }
+     })
+ }
 }
 
 module.exports = Query;
